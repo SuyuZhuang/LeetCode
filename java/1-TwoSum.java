0ec3bab55fc832a398b8d030ce7f123974ea0a8e
Suyu <zhuangsuyu1993@163.com>
2017-11-12 22:45:05 -0600

    Update 1-TwoSum.java

--- java/1-TwoSum.java
+++ java/1-TwoSum.java
@@ -1,5 +1,5 @@
-// approach1
-public int[] twoSum(int[] nums, int target) {
+// approach1, brute force, 暴力算法
+public int[] twoSum1(int[] nums, int target) {
     for (int i = 0; i < nums.length; i++) {
         for (int j = i + 1; j < nums.length; j++) {
             if (nums[j] == target - nums[i]) {
@@ -8,4 +8,21 @@ public int[] twoSum(int[] nums, int target) {
         }
     }
     throw new IllegalArgumentException("No two sum solution");
+}
+
+// approach2, Two-pass Hash Table
+class Solution {
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        for (int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i){
+                return new int[]{i,map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("no two sum solution");
+    }
 }
