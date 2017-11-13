b416bd3bf8f16ea6d7bcd9e60b72af2ac007126c
Suyu <zhuangsuyu1993@163.com>
2017-11-12 22:50:58 -0600

    Update 1-TwoSum.java

--- java/1-TwoSum.java
+++ java/1-TwoSum.java
@@ -26,3 +26,19 @@ class Solution {
         throw new IllegalArgumentException("no two sum solution");
     }
 }
+
+// approach3, One-pass Hash Table
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+       
+        for (int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i){
+                return new int[]{map.get(complement),i};
+            }
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("no two sum solution");
+    }
+}
