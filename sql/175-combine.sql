945089ead68182a5774d6f92a9c1012d389ee584
Suyu <zhuangsuyu1993@163.com>
2017-11-13 18:49:19 -0600

    Update 175-combine.sql

--- sql/175-combine.sql
+++ sql/175-combine.sql
@@ -1,5 +1,5 @@
 -- Tabel:Person(PersonId,FirstName,LastName)
 -- Table:Address(AddressId,PersonId,City,State)
 
-# Write your MySQL query statement below
+-- 971ms
 SELECT p.FirstName,p.LastName,a.City,a.State From Person as p LEFT JOIN Address AS a on P.PersonID = a.PersonId;
