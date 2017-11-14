30382aaff086f17922e19acd8ba1bca446afc575
Suyu <zhuangsuyu1993@163.com>
2017-11-13 19:29:57 -0600

    Update 2-addTwoNums.java

--- java/2-addTwoNums.java
+++ java/2-addTwoNums.java
@@ -68,3 +68,27 @@ class Solution {
         return dummyRoot.next;
     }
 }
+
+
+
+// 官方答案，代码更简洁，50ms
+ ListNode dummyNode = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyNode;
+        int carry = 0;
+        while (p != null || q != null){
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int temp = x + y + carry;
+            carry = temp/10;
+            curr.next = new ListNode(temp % 10);
+            curr = curr.next;
+            
+            if (p != null){p = p.next;}
+            if (q != null){q = q.next;}
+        }
+        
+        if (carry > 0){
+            curr.next = new ListNode(carry);
+        }
+        
+        return dummyNode.next;
