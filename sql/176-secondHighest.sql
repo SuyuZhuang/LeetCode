17bc633de3025b0bddeba56842933509267a4dca
Suyu <zhuangsuyu1993@163.com>
2017-11-13 18:48:45 -0600

    Update 176-secondHighest.sql

--- sql/176-secondHighest.sql
+++ sql/176-secondHighest.sql
@@ -1,3 +1,6 @@
-# Table: employee(Id,Salary), find the second Highest Salary In employee table
-
+-- Table: employee(Id,Salary), find the second Highest Salary In employee table
+-- use NOT IN, 1292ms
 SELECT Max(Salary) as SecondHighestSalary FROM Employee WHERE Salary NOT IN (SELECT Max(Salary) FROM Employee);
+
+-- use < , 938ms
+SELECT Max(Salary) as SecondHighestSalary FROM Employee WHERE Salary < (SELECT Max(Salary) FROM Employee);
